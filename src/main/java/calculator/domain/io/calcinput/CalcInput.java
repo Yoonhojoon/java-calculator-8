ecca0455bc2350323d2e1cf7f8e878d375147ff4
Yoonhojoon <dbsghwns1209@gmail.com>
2025-10-16 15:35:11 +0900

    feat(문자열 입력 기능): 문자열 입력 객체인 CalcInput 구현 및 테스트 코드 작성
    문자열을 입력받고, 입력값이 없을 시 예외 반환하는 기능 구현

---
+++ src/main/java/calculator/domain/io/calcinput/CalcInput.java
@@ -0,0 +1,27 @@
+package calculator.domain.io.calcinput;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class CalcInput {
+    private String input;
+
+    public CalcInput(String input) {
+        validateInput(input);
+        this.input = input;
+    }
+
+    private void validateInput(String input) {
+        if (input == null || input.isEmpty()) {
+            throw new IllegalArgumentException("input cannot be null or empty");
+        }
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public static String read() {
+        return Console.readLine();
+    }
+
+}
---
+++ src/test/java/calculator/domain/io/calcinput/CalcInputTest.java
@@ -0,0 +1,30 @@
+package calculator.domain.io.calcinput;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CalcInputTest {
+
+    @Test
+    @DisplayName("빈 문자열 입력 시 IllegalArgumentException 반환")
+    void calcInput_fail() {
+        //given&&when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {new CalcInput("");});
+        //then
+        assertEquals("input cannot be null or empty", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("성공적 문자열 입력 시 CalcInput 객체 반환")
+    void calcInput_success() {
+        //given
+        CalcInput calcInput = new CalcInput("//123");
+        //when&then
+        assertEquals("//123", calcInput.getInput());
+    }
+
+}
